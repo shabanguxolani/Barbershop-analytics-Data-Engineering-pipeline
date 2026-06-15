e68bc67c9612cefd6b8ae9d4ac4d841f22a9164a
Xolani <shabanguxolani35@gmail.com>
2026-06-15 19:32:31 +0200

    Create Staginge table

--- create_tables.sql
+++ create_tables.sql
@@ -123,3 +123,151 @@ CREATE TABLE bronze_payments_raw (			-- payments table
 	loaded_at DATETIME DEFAULT GETDATE()
 );
 GO
+
+--======================================================
+-- CREATE STAGING TABLES
+--======================================================
+--Customers 
+
+IF OBJECT_ID('staging.customers', 'U') IS NOT NULL
+    DROP TABLE staging.customers;
+GO
+
+CREATE TABLE staging.customers (
+    customer_id VARCHAR(50),
+    first_name VARCHAR(200),
+    last_name VARCHAR(50),
+    full_name VARCHAR(250),
+    phone_number VARCHAR(50),
+    email VARCHAR(200),
+    email_domain VARCHAR(100),
+    gender VARCHAR(50),
+    gender_group VARCHAR(20),
+    city VARCHAR(50),
+    province VARCHAR(50),
+    region VARCHAR(50),
+    signup_date DATE,
+    signup_year INT,
+    source_file_name VARCHAR(200),
+    loaded_at DATETIME2
+);
+GO
+
+-- BARBERS
+IF OBJECT_ID('staging.barbers', 'U') IS NOT NULL
+    DROP TABLE staging.barbers;
+GO
+
+CREATE TABLE staging.barbers (
+    barber_code VARCHAR(20),
+    barber_name VARCHAR(100),
+    branch_code VARCHAR(20),
+    branch_city_code VARCHAR(10),
+    experience_level VARCHAR(20),
+    experience_group VARCHAR(30),
+    hire_date DATE,
+    years_employed INT,
+    source_file_name VARCHAR(200),
+    loaded_at DATETIME2
+);
+GO
+
+-- BRANCHES
+IF OBJECT_ID('staging.branches', 'U') IS NOT NULL
+    DROP TABLE staging.branches;
+GO
+
+CREATE TABLE staging.branches (
+    branch_code VARCHAR(50),
+    branch_name VARCHAR(200),
+    branch_city_code VARCHAR(10),
+    branch_suburb_code VARCHAR(10),
+    province VARCHAR(100),
+    city VARCHAR(100),
+    suburb VARCHAR(50),
+    region VARCHAR(50),
+    source_file_name VARCHAR(200),
+    loaded_at DATETIME2
+);
+GO
+
+--SERVICES
+IF OBJECT_ID('staging.services', 'U') IS NOT NULL
+    DROP TABLE staging.services;
+GO
+
+CREATE TABLE staging.services (
+    service_code VARCHAR(50),
+    service_short_code VARCHAR(50),
+    [service_name] VARCHAR(200),
+    category VARCHAR(100),
+    service_group VARCHAR(100),
+    standard_price DECIMAL(10,2),
+    duration_minutes INT,
+    duration_bucket VARCHAR(50),
+    price_bucket VARCHAR(50),
+    source_file_name VARCHAR(200),
+    loaded_at DATETIME2
+);
+GO
+
+-- BOOKINGS
+IF OBJECT_ID('staging.bookings', 'U') IS NOT NULL
+    DROP TABLE staging.bookings;
+GO
+
+CREATE TABLE staging.bookings (
+    booking_id VARCHAR(50),
+    customer_id VARCHAR(50),
+    barber_code VARCHAR(50),
+    service_code VARCHAR(50),
+    service_short_code VARCHAR(50),
+    branch_code VARCHAR(50),
+    branch_city_code VARCHAR(10),
+    branch_suburb_code VARCHAR(10),
+    booking_datetime DATETIME2,
+    booking_date DATE,
+    booking_year INT,
+    booking_month INT,
+    booking_month_name VARCHAR(20),
+    booking_day_name VARCHAR(20),
+    booking_hour INT,
+    time_of_day_bucket VARCHAR(20),
+    is_weekend BIT,
+    booking_status VARCHAR(50),
+    is_completed BIT,
+    is_cancelled BIT,
+    is_no_show BIT,
+    gross_amount DECIMAL(10,2),
+    discount_amount DECIMAL(10,2),
+    tip_amount DECIMAL(10,2),
+    net_amount DECIMAL(10,2),
+    has_discount BIT,
+    has_tip BIT,
+    source_file_name VARCHAR(200),
+    loaded_at DATETIME2
+);
+GO
+
+-- PAYMENTS
+IF OBJECT_ID('staging.payments', 'U') IS NOT NULL
+    DROP TABLE staging.payments;
+GO
+
+CREATE TABLE staging.payments (
+    payment_id VARCHAR(50),
+    booking_id VARCHAR(50),
+    payment_method VARCHAR(50),
+    payment_method_group VARCHAR(50),
+    payment_amount DECIMAL(10,2),
+    payment_status VARCHAR(50),
+    is_successful_payment BIT,
+    is_failed_payment BIT,
+    is_reversed_payment BIT,
+    payment_datetime DATETIME2,
+    payment_date DATE,
+    payment_hour INT,
+    source_file_name VARCHAR(200),
+    loaded_at DATETIME2
+);
+GO
