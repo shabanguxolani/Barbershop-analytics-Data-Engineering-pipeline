038de5d9e798792bbe9f0e17f0a6928fc01c50a7
Xolani <shabanguxolani35@gmail.com>
2026-06-20 10:54:22 +0200

    Update Load_stating.sql

--- load_staging.sql
+++ load_staging.sql
@@ -1,3 +1,104 @@
+USE Barbershop_analytics;
+GO
+
+-- Staging Bookings
+
+TRUNCATE TABLE staging.bookings;
+
+INSERT INTO staging.bookings (
+    booking_id, customer_id, barber_code, service_code, service_short_code,branch_code, 
+    branch_city_code, branch_suburb_code, booking_datetime, booking_date, booking_year,
+    booking_month, booking_month_name, booking_day_name, booking_hour, time_of_day_bucket,
+    is_weekend, booking_status, is_completed, is_cancelled, is_no_show, gross_amount,
+    discount_amount, tip_amount, net_amount, has_discount, has_tip , source_file_name, loaded_at
+)
+SELECT
+    NULLIF(TRIM(booking_id), ''),
+    COALESCE(NULLIF(UPPER(TRIM(customer_id)), ''), 'UNKNOWN'),
+    COALESCE(NULLIF(UPPER(TRIM(barber_code)), ''), 'UNKNOWN'),
+    CASE
+        WHEN service_code IS NULL OR TRIM(service_code) = '' THEN 'UNKNOWN'
+        WHEN UPPER(TRIM(service_code)) = 'SVC-UNKNOWN' THEN 'UNKNOWN'
+    ELSE
+        UPPER(service_code)
+    END,
+    CASE
+        WHEN service_code IS NULL OR TRIM(service_code) = '' THEN 'UNKNOWN'
+        WHEN UPPER(TRIM(service_code)) = 'SVC-UNKNOWN' THEN 'UNKNOWN'
+    ELSE
+        REPLACE(UPPER(TRIM(service_code)), 'SVC-', '')
+    END,
+    COALESCE(NULLIF(UPPER(TRIM(branch_code)), ''), 'UNKNOWN'),
+    CASE
+        WHEN branch_code IS NULL OR TRIM(branch_code) = '' THEN 'UNKNOWN'
+    ELSE
+        SUBSTRING(UPPER(TRIM(branch_code)), 4, 3)
+    END,
+    CASE   
+        WHEN branch_code IS NULL OR TRIM(branch_code) = '' THEN 'UNKNOWN'
+    ELSE
+        RIGHT(UPPER(TRIM(branch_code)), 3)
+    END,
+    TRY_CONVERT(DATETIME2, booking_datetime),
+    CAST(TRY_CONVERT(DATETIME2, booking_datetime) AS DATE),
+    DATEPART(YEAR, TRY_CONVERT(DATETIME2, booking_datetime)),
+    DATEPART(MONTH, TRY_CONVERT(DATETIME2, booking_datetime)),
+    DATENAME(MONTH, TRY_CONVERT(DATETIME2, booking_datetime)),
+    DATENAME(WEEKDAY, TRY_CONVERT(DATETIME2, booking_datetime)),
+    DATEPART(HH, TRY_CONVERT(DATETIME2, booking_datetime)),
+    CASE
+        WHEN DATEPART(HH, TRY_CONVERT(DATETIME2, booking_datetime)) BETWEEN 6 AND 11 THEN 'Morning'
+        WHEN DATEPART(HH, TRY_CONVERT(DATETIME2, booking_datetime)) BETWEEN 12 AND 16 THEN 'Afternoon'
+        WHEN DATEPART(HH, TRY_CONVERT(DATETIME2, booking_datetime)) BETWEEN 17 AND 20 THEN 'Evening'
+    ELSE   
+        'Other'
+    END,
+    CASE
+        WHEN DATENAME(WEEKDAY, TRY_CONVERT(DATETIME2, booking_datetime)) IN ('Saturday','Sunday') THEN 1
+    ELSE
+        0
+    END,
+    COALESCE(NULLIF(TRIM(booking_status), ''), 'UNKNOWN'),
+    CASE   
+        WHEN booking_status = 'Completed' THEN 1
+    ELSE
+        0
+    END,
+    CASE 
+        WHEN booking_status = 'Cancelled' THEN 1
+    ELSE
+        0
+    END,
+    CASE    
+       WHEN booking_status = 'No Show' THEN 1
+    ELSE
+        0
+    END,
+    TRY_CONVERT(DECIMAL(10, 2), booking_amount),
+    TRY_CONVERT(DECIMAL(10, 2), discount_amount),
+    TRY_CONVERT(DECIMAL(10, 2), tip_amount),
+    ISNULL(TRY_CONVERT(DECIMAL(10, 2), booking_amount),0)
+    - ISNULL(TRY_CONVERT(DECIMAL(10, 2), discount_amount), 0)
+    + ISNULL(TRY_CONVERT(DECIMAL(10, 2), tip_amount), 0),
+    CASE
+        WHEN ISNULL(TRY_CONVERT(DECIMAL(10, 2), discount_amount),0) > 0 THEN 1
+    ELSE
+        0
+    END,
+    CASE
+        WHEN ISNULL(TRY_CONVERT(DECIMAL(10, 2), tip_amount),0) > 0 THEN 1
+    ELSE
+        0
+    END,
+    source_file_name,
+    loaded_at
+
+FROM bronze.bookings_raw
+
+SELECT * FROM staging.bookings
+
+-- Staging Payments 
+
 TRUNCATE TABLE staging.payments;
 
 INSERT INTO staging.payments (
@@ -28,6 +129,9 @@ SELECT
 FROM bronze.payments_raw;
 GO
 
+-- SELECT * FROM staging.payments
+
+-- Staging Branches
 
 TRUNCATE TABLE staging.branches;
 
@@ -53,6 +157,9 @@ SELECT
 FROM bronze.branches_raw;
 GO
 
+-- SELECT * FROM staging.branches
+
+-- Staging Services
 
 TRUNCATE TABLE staging.services;
 
@@ -88,6 +195,9 @@ SELECT
 FROM bronze.services_raw;
 GO
 
+-- SELECT * FROM staging.[services]
+
+-- Staging Barbers
 
 TRUNCATE TABLE staging.barbers;
 
@@ -115,6 +225,9 @@ SELECT
 FROM bronze.barbers_raw;
 GO
 
+-- SELECT * FROM staging.barbers
+
+-- Staging Customers
 
 TRUNCATE TABLE staging.customers;
 
@@ -155,5 +268,4 @@ SELECT
 FROM bronze.customers_raw;
 GO
 
-
-SELECT * FROM staging.payments
+-- SELECT * FROM staging.customers
